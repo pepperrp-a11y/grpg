11ef2c3e3db9a53c0d444892823093968f43b43f
John Doe <pepperrp@gmail.com>
2026-05-28 23:17:15 -0400

    working better lots of slop tho

---
+++ CMakeFiles/sphere.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sphere.dir/main.cpp.obj"
+  "CMakeFiles/sphere.dir/main.cpp.obj.d"
+  "libsphere.dll.a"
+  "sphere.exe"
+  "sphere.exe.manifest"
+  "sphere.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sphere.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
